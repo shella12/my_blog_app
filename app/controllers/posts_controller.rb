8b0ee7a9390f5ecc61ae568504ab70aa50acce4c
shella12 <ayeshaarshad4567@gmail.com>
2023-03-17 21:50:23 +0500

    Add user index view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,6 +4,6 @@ class PostsController < ApplicationController
   end
 
   def show
-    # @postId = Post.find(params[:id])
+    @post = Post.find(params[:id])
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,6 @@ class UsersController < ApplicationController
   end
 
   def show
-    # @user = User.find(params[:id]).name
+    @user = User.find(params[:id])
   end
 end
