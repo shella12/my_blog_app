db894071c3700f662594e6c7827dea993995e9ed
shella12 <ayeshaarshad4567@gmail.com>
2023-03-17 10:42:28 +0500

    Add controllers and actions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,3 +1,9 @@
 class PostsController < ApplicationController
-  
+  def index
+    @posts = Post.all
+  end
+
+  def show
+    # @postId = Post.find(params[:id])
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,10 @@
 class UsersController < ApplicationController
-  
+  def index
+    @users = User.all
+    @username = 'ayesha'
+  end
+
+  def show
+    # @user = User.find(params[:id]).name
+  end
 end
