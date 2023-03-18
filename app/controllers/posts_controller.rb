a54dfcabfa664549fdcfc65c074d030d30ce499c
shella12 <ayeshaarshad4567@gmail.com>
2023-03-18 18:23:26 +0500

    Add post views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,17 @@
 class PostsController < ApplicationController
+  
   def index
-    @posts = Post.all
+    @user = User.find(params[:user_id])
+    @posts = Post.where(user_id: params[:user_id])
   end
 
   def show
     @post = Post.find(params[:id])
+    @comments = Comment.where(post_id: params[:id])
   end
+
+  def username(user_id)
+      User.find(user_id).name
+  end
+  helper_method :username
 end
