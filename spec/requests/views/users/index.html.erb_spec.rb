b56f5a2b8b32289bb35a67861e0637c6aa06a14c
shella12 <ayeshaarshad4567@gmail.com>
2023-03-30 11:00:27 +0500

    Add test for users index

--- spec/requests/views/users/index.html.erb_spec.rb
+++ spec/requests/views/users/index.html.erb_spec.rb
@@ -1,14 +1,19 @@
 require 'rails_helper'
 
-RSpec.describe 'users/index.html.erb', type: :view do
+RSpec.describe 'users/index.html.erb', type: :system do
   before do
-    render
-  end
-
-  it 'p tag is present' do
-    expect(rendered).to have_selector('p')
-  end
-  it 'p has a placeholder' do
-    expect(rendered).to include('Hi I\'m a user my name is')
+    @current_user = User.first
+    end
+  describe 'index page' do
+    it 'shows the right content' do
+      visit users_path
+      expect(page).to have_selector('div.user', count: (User.all.length.to_i - 1))
+      expect(page).to have_selector('img.profile_photo', count: (User.all.length.to_i))
+      User.order(:id).offset(1).each do |user|
+        expect(page).to have_selector('p',text: "Number of posts: " + (user.posts_counter).to_s)  
+      end
+      click_link(User.first.name)
+      expect(current_path). to eql('/users')
+    end
   end
 end
