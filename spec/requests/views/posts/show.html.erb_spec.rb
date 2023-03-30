77a6987334480cb4d48f24a518576eadf8725291
shella12 <ayeshaarshad4567@gmail.com>
2023-03-30 14:28:41 +0500

    Add post show tests

--- spec/requests/views/posts/show.html.erb_spec.rb
+++ spec/requests/views/posts/show.html.erb_spec.rb
@@ -1,14 +1,20 @@
 require 'rails_helper'
 
-RSpec.describe 'posts/show.html.erb', type: :view do
+RSpec.describe 'posts/show.html.erb', type: :system do
   before do
-    render
-  end
-
-  it 'p tag is present' do
-    expect(rendered).to have_selector('p')
-  end
-  it 'p has a placeholder' do
-    expect(rendered).to include('This is my post and i\'m a user id')
+    @post = Post.find(3)
+    end
+  describe 'index page' do
+    it 'shows the right content' do
+      visit ('/users/1/posts/3')
+      expect(page).to have_selector('h1', text: "Post#3 by #{@post.user.name}")
+      expect(page).to have_content("likes: #{@post.likes_counter}")   
+      expect(page).to have_content("comments: #{@post.comments_counter}")   
+      expect(page).to have_selector('h2',text: (@post.title).strip)  
+      expect(page).to have_selector('p',text: @post.text)  
+      @post.comments.each do |comment|
+        expect(page).to have_selector('li', text: "#{(comment.user.name)}: #{(comment.text)}")      
+      end
+    end
   end
 end
