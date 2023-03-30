0bb57ea52c2cc580db2b8673cb3b98a874ebdf86
shella12 <ayeshaarshad4567@gmail.com>
2023-03-30 14:33:14 +0500

    Fix linter errors

--- spec/views/posts/index.html.erb_spec.rb
+++ spec/views/posts/index.html.erb_spec.rb
@@ -4,19 +4,19 @@ RSpec.describe 'posts/index.html.erb', type: :system do
   before do
     @user = User.first
     @posts = @user.posts.order(:id)
-    end
+  end
   describe 'index page' do
     it 'shows the right content' do
-      visit ('/users/1/posts')
+      visit('/users/1/posts')
       expect(page).to have_selector('img.profile_photo')
       expect(page).to have_selector('h1', text: @user.name)
-      expect(page).to have_selector('p',text: "Number of posts: #{@user.posts_counter.to_s}")    
+      expect(page).to have_selector('p', text: "Number of posts: #{@user.posts_counter}")
       @posts.each do |post|
-        expect(page).to have_selector('h2',text: (post.title).strip)  
-        expect(page).to have_selector('p',text: post.text)
-        expect(page).to have_content(post.comments.first.text) unless post.comments.first.nil? 
-        expect(page).to have_content("likes: #{post.likes_counter}")   
-        expect(page).to have_content("comments: #{post.comments_counter}")    
+        expect(page).to have_selector('h2', text: post.title.strip)
+        expect(page).to have_selector('p', text: post.text)
+        expect(page).to have_content(post.comments.first.text) unless post.comments.first.nil?
+        expect(page).to have_content("likes: #{post.likes_counter}")
+        expect(page).to have_content("comments: #{post.comments_counter}")
       end
       click_link("Post##{@posts.first.id}")
       sleep(1)
