7596fb2581f2708a4c016b960e4d48023c10bd31
shella12 <ayeshaarshad4567@gmail.com>
2023-03-30 12:45:43 +0500

    Add users show tests

--- spec/requests/views/users/show.html.erb_spec.rb
+++ spec/requests/views/users/show.html.erb_spec.rb
@@ -1,14 +1,28 @@
 require 'rails_helper'
 
-RSpec.describe 'users/show.html.erb', type: :view do
+RSpec.describe 'users/show.html.erb', type: :system do
   before do
-    render
-  end
-
-  it 'p tag is present' do
-    expect(rendered).to have_selector('p')
-  end
-  it 'p has a placeholder' do
-    expect(rendered).to include('Hi I\'m a user with an id')
+    @user = User.first
+    @posts = @user.most_recent_posts
+    end
+  describe 'show page' do
+    it 'shows the right content' do
+      visit ('/users/1')
+      expect(page).to have_selector('h1', text: @user.name)
+      expect(page).to have_selector('img.profile_photo')
+      expect(page).to have_selector('p',text: "Number of posts: " + (@user.posts_counter).to_s)  
+      expect(page).to have_selector('p',text: (@user.bio).to_s)    
+      expect(page).to have_link('See all posts', href: user_posts_path(@user.id)) 
+      @posts.each do |post| 
+      expect(page).to have_selector('p',text: (post.text).to_s) 
+      end
+      click_link('Post#' + @posts.first.id.to_s)
+      sleep(1)
+      expect(current_path). to eql('/users/'+(@user.id).to_s+'/posts/'+(@posts.first.id).to_s)
+      page.go_back
+      click_link('See all posts')
+      sleep(1)
+      expect(current_path). to eql(user_posts_path(@user.id))
+    end
   end
 end
